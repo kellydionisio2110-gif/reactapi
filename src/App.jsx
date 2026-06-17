c900e2deafc4b3f3e2020c9a13e239852a9f7558
kellydionisio2110-gif <kelly.dionisio2110@gmail.com>
2026-06-17 17:01:04 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+
+import s from './App.module.css'
+import logo from '/logo.svg'
+import { api } from './constants/api'
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [data, setData] = useState([])
+  const [page, setPage] = useState()
+  const [inputPage, setInputPage] = useState("")
+
+  useEffect(() => {
+    const carrega = async () => {
+      try {
+        const response = await api.get(`/character?page=${page}`)
+        setData(response.data.results)
+      } catch {
+        console.error("Não foi possível buscar personagens")
+
+      }
+    }
+    carrega()
+
+    return () => {
+
+    }
+  }, [])
+
+
+
+  return (
+    <>
+
+      <div className={s.wrapImg}>
+        <img src={logo} alt="logo" className={s.logo} />
+      </div>
+      <div clasName={s.wrapInputs}>
+        <label>Digitie uma página de 1/42</label>
+        <input type="number" min={1} max={42} placeholder='1/42' value={inputPage} onChange={(e) => setInputPage(e.target.value)} />
+        <button onClick={() => setPage(Number(inputPage))}>BUSCAR</button>
+      </div>
+
+      <main>
+        {data.map(item => {
+          return (
+            <div key={item.id} className={s.card}>
+              <img src={item.image} alt={item.name}/>
+              <div className={s.wrap/texts}>
+                <h2>{item.name}</h2>
+                <p>{item.status}</p>
+                <p>{item.species}</p>
+                <p>{item.gender}</p>
+                <p>{item.location.name}</p>
+              </div>
+            </div>
+          )
+        })}
+
+      </main>
+
+    </>
+
+
+
+
+
+  )
+}
+
+export default App
